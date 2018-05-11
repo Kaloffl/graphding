6631d5be9218f4cf09c5db2c8eb8256e5eff7acf
Lars Donner <lars.donner@gmail.com>
2018-05-11 20:07:47 +0200

    Implemented better rendering algorithm for SDFs

--- src/SdfRenderer.scala
+++ src/SdfRenderer.scala
@@ -68,22 +68,22 @@ object SdfRenderer {
 
     // Get a sign which way points "outside" on each side.
     // This is necessary to make the inputs winding independent.
-    val s21 = if (dx21 * dy32 < dy21 * dx32)  1 else -1
-    val s32 = if (dx32 * dy13 < dy32 * dx13)  1 else -1
-    val s13 = if (dx13 * dy21 < dy13 * dx21)  1 else -1
-    
+    val s21 = if (dx21 * dy32 < dy21 * dx32) 1 else -1
+    val s32 = if (dx32 * dy13 < dy32 * dx13) 1 else -1
+    val s13 = if (dx13 * dy21 < dy13 * dx21) 1 else -1
+
     // Calculate the inverse length and normal vectors that point away from the triangle on each side.
-    val il21 = 1.0 / sqrt(dx21 * dx21 + dy21 * dy21)
-    val nx21 = (-dy21 * il21).toFloat * s21
-    val ny21 = ( dx21 * il21).toFloat * s21
+    val il21 = s21 / sqrt(dx21 * dx21 + dy21 * dy21)
+    val nx21 = (-dy21 * il21).toFloat
+    val ny21 = ( dx21 * il21).toFloat
 
-    val il32 = 1.0 / sqrt(dx32 * dx32 + dy32 * dy32)
-    val nx32 = (-dy32 * il32).toFloat * s32
-    val ny32 = ( dx32 * il32).toFloat * s32
+    val il32 = s32 / sqrt(dx32 * dx32 + dy32 * dy32)
+    val nx32 = (-dy32 * il32).toFloat
+    val ny32 = ( dx32 * il32).toFloat
 
-    val il13 = 1.0 / sqrt(dx13 * dx13 + dy13 * dy13)
-    val nx13 = (-dy13 * il13).toFloat * s13
-    val ny13 = ( dx13 * il13).toFloat * s13
+    val il13 = s13 / sqrt(dx13 * dx13 + dy13 * dy13)
+    val nx13 = (-dy13 * il13).toFloat
+    val ny13 = ( dx13 * il13).toFloat
 
     // Calculate the distance from the origin to the line going through each vertex pair.
     val d21 = x1 * nx21 + y1 * ny21
@@ -106,7 +106,7 @@ object SdfRenderer {
   // takes a SDF and returns a SDF with the given translation applied
   def translate(sdf: Sdf, translate_x: Float, translate_y: Float): Sdf = (x, y) => sdf(x + translate_x, y + translate_y)
 
-  // takes a SDF and returns a SDF with the given scaling applied  
+  // takes a SDF and returns a SDF with the given scaling applied
   def scale(sdf: Sdf, scale_x: Float, scale_y: Float): Sdf = (x, y) => sdf(x * scale_x, y * scale_y)
 
   // takes a SDF and returns one rotatet clockwise by the given angle around the given pivot
@@ -153,89 +153,143 @@ object SdfRenderer {
   // SHAPE RENDERING
   //
 
-  private val half_sqrt_2 = sqrt(2) / 2
+  private val half_sqrt_2 = sqrt(2).toFloat * 0.5f
+  private val inverse_sqrt_2 = (1.0 / sqrt(2)).toFloat
+
+  def coverage(
+    left: Float, top: Float,
+    size: Float,
+    max_depth: Int,
+    sdf: Sdf
+  ): Float = {
+    val half_size = size * 0.5f
+    val center_x = left + half_size
+    val center_y = top  + half_size
+
+    val dist = sdf(center_x, center_y)
+
+    if (0 == max_depth) {
+      if (dist <= 0) return size * size
+      return 0.0f
+    }
+
+    val half_diag = size * half_sqrt_2
+
+    if ( dist >= half_diag) return 0.0f
+    if (-dist >= half_diag) return size * size
+    return coverage(    left,      top, half_size, max_depth - 1, sdf) +
+           coverage(center_x,      top, half_size, max_depth - 1, sdf) +
+           coverage(    left, center_y, half_size, max_depth - 1, sdf) +
+           coverage(center_x, center_y, half_size, max_depth - 1, sdf)
+  }
+
+  def render(
+    target: RenderTarget,
+    left: Int, top: Int,
+    width: Int, height: Int,
+    sdf: Sdf,
+    color: Color,
+    alpha: Float
+  ): Unit = {
+
+    if (width <= 3 && height <= 3) {
+      for (y <- 0 until height; x <- 0 until width) {
+        val c = coverage(left - 0.5f + x, top - 0.5f + y, 1.0f, 4, sdf)
+        target.set_pixel(left + x, top + y, color, c * alpha)
+      }
+    } else {
+      val half_width = width * 0.5f
+      val half_height = height * 0.5f
+      val center_x = left + half_width - 0.5f
+      val center_y = top + half_height - 0.5f
+
+      val dist = sdf(center_x, center_y)
+      val abs_dist = abs(dist)
+      val dist_squared = dist * abs_dist
+      val half_diag_squared = (half_width * half_width + half_height * half_height)
+
+
+      if (dist_squared < half_diag_squared) {
+        if (-dist_squared >= half_diag_squared) {
+          target.fill_rect(left, top, left + width - 1, top + height - 1, color, alpha)
+        } else {
+          if (half_width < abs_dist || half_height < abs_dist) {
+            if (height < width) {
+              val    left_width = ceil(half_width - sqrt(dist * dist - half_height * half_height)).toInt
+              val  center_width = max(0, width - 2 * left_width)
+              val   right_width = width - left_width - center_width
+              val      center_x = left + left_width
+              val         right = center_x + center_width
+
+              render(target,  left, top,  left_width, height, sdf, color, alpha)
+              render(target, right, top, right_width, height, sdf, color, alpha)
+
+              if (dist < 0 && 0 < center_width) {
+                target.fill_rect(center_x, top, right - 1, height + top - 1, color, alpha)
+              }
+            } else {
+              val    top_height = ceil(half_height - sqrt(dist * dist - half_width * half_width)).toInt
+              val center_height = max(0, height - 2 * top_height)
+              val bottom_height = height - top_height - center_height
+              val      center_y = top  + top_height
+              val        bottom = center_y + center_height
+
+              render(target, left,    top, width,    top_height, sdf, color, alpha)
+              render(target, left, bottom, width, bottom_height, sdf, color, alpha)
+
+              if (dist < 0 && 0 < center_height) {
+                target.fill_rect(left, center_y, left + width - 1, bottom - 1, color, alpha)
+              }
+            }
+          } else {
+            val half_inner_size = abs_dist * inverse_sqrt_2
+
+            val    left_width = ceil(half_width - half_inner_size).toInt
+            val  center_width = max(0, width - 2 * left_width)
+            val   right_width = width - left_width - center_width
+            val      center_x = left + left_width
+            val         right = center_x + center_width
+
+            val    top_height = ceil(half_height - half_inner_size).toInt
+            val center_height = max(0, height - 2 * top_height)
+            val bottom_height = height - top_height - center_height
+            val      center_y = top  + top_height
+            val        bottom = center_y + center_height
+
+            if (height < width) {
+              render(target,  left, top,  left_width, height, sdf, color, alpha)
+              render(target, right, top, right_width, height, sdf, color, alpha)
+
+              if (0 < center_width && 0 < top_height) {
+                render(target, center_x,    top, center_width,    top_height, sdf, color, alpha)
+                render(target, center_x, bottom, center_width, bottom_height, sdf, color, alpha)
+              }
+            } else {
+              render(target, left,    top, width,    top_height, sdf, color, alpha)
+              render(target, left, bottom, width, bottom_height, sdf, color, alpha)
+
+              if (0 < center_height && 0 < left_width) {
+                render(target,  left, center_y,  left_width, center_height, sdf, color, alpha)
+                render(target, right, center_y, right_width, center_height, sdf, color, alpha)
+              }
+            }
+
+            if (dist < 0 && 0 < center_width && 0 < center_height) {
+              target.fill_rect(center_x, center_y, right - 1, bottom - 1, color, alpha)
+            }
+          }
+        }
+      }
+      //target.draw_rect(left, top, left + width, top + height, 1, Color.Red, 0.5f)
+    }
+  }
 
   def render(
     target: RenderTarget,
     sdf: Sdf,
     color: Color,
-    alpha: Float = 1f,
-    multisampling: Int = 16
+    alpha: Float = 1f
   ): Unit = {
-
-    val d1 = sdf(           0,             0)
-    val d2 = sdf(           0, target.height)
-    val d3 = sdf(target.width,             0)
-    val d4 = sdf(target.width, target.height)
-
-    val screen_diag_square = target.width * target.width + target.height * target.height
-
-    if (d1 * abs(d1) > screen_diag_square ||
-        d2 * abs(d2) > screen_diag_square ||
-        d3 * abs(d3) > screen_diag_square ||
-        d4 * abs(d4) > screen_diag_square) {
-      return
-    }
-
-    val det1 = (-target.height + d2 - d1) * (-target.height - d2 + d1) * (-target.height + d2 + d1) * (target.height + d2 + d1)
-    val det2 = (-target.height + d4 - d3) * (-target.height - d4 + d3) * (-target.height + d4 + d3) * (target.height + d4 + d3)
-    val det3 = (-target.width + d3 - d1) * (-target.width - d3 + d1) * (-target.width + d3 + d1) * (target.width + d3 + d1)
-    val det4 = (-target.width + d4 - d2) * (-target.width - d4 + d2) * (-target.width + d4 + d2) * (target.width + d4 + d2)
-
-    val min_x = if (d1 > 0 && d2 > 0 && det1 > 0) floor(               sqrt(det1) / target.height / 2).toInt else 0
-    val max_x = if (d3 > 0 && d4 > 0 && det2 > 0) ceil( target.width - sqrt(det2) / target.height / 2).toInt else target.width
-    val min_y = if (d1 > 0 && d3 > 0 && det3 > 0) floor(               sqrt(det3) / target.width  / 2).toInt else 0
-    val max_y = if (d2 > 0 && d4 > 0 && det4 > 0) ceil(target.height - sqrt(det4) / target.width  / 2).toInt else target.height
-
-    //target.draw_rect(min_x, min_y, max_x, max_y, 1, Color.Red)
-
-    val half_ms = multisampling / 2
-    val inverse_square_ms = 1f / (multisampling * multisampling)
-
-    var pixel_y = min_y
-    while (pixel_y < max_y) {
-
-      var pixel_x = min_x
-      while (pixel_x < max_x) {
-        val dist = sdf(pixel_x, pixel_y)
-
-        if (half_sqrt_2 <= dist) {
-          pixel_x += max(1, dist.toInt)
-          //target.set_pixel(pixel_x, pixel_y, Color.Green, 0.5f)
-        } else {
-          if (dist <= -half_sqrt_2) {
-            val end_range = pixel_x + max(1, (-dist).toInt)
-            while (pixel_x < end_range) {
-              target.set_pixel(pixel_x, pixel_y, color, alpha)
-              pixel_x += 1
-            }
-          } else {
-            var coverage = 0f
-            var multisampling_y = -half_ms
-            while (multisampling_y < half_ms) {
-              val subpixel_y = pixel_y + (multisampling_y + 0.5f) / multisampling
-              var multisampling_x = -half_ms
-              while (multisampling_x < half_ms) {
-                val subpixel_x = pixel_x + (multisampling_x + 0.5f) / multisampling
-
-                val dist = sdf(subpixel_x, subpixel_y)
-                if (dist <= 0) {
-                  val range = max(1, min(half_ms - multisampling_x, (-dist * multisampling).toInt))
-                  coverage += range * inverse_square_ms
-                  multisampling_x += range
-                } else {
-                  multisampling_x += max(1, (dist * multisampling).toInt)
-                }
-              }
-              multisampling_y += 1
-            }
-            target.set_pixel(pixel_x, pixel_y, color, alpha * coverage)
-            pixel_x += 1
-          }
-        }
-      }
-
-      pixel_y += 1
-    }
+    render(target, 0, 0, target.width, target.height, sdf, color, alpha)
   }
 }
